be0da580f08a7d4c1b0b1f71a9bd311d0f948d1d
Jake Greer <jake.greer@instride.com>
2023-05-15 14:56:50 -0700

    liars-dice updates

--- src/commands/General/liarsDice.ts
+++ src/commands/General/liarsDice.ts
@@ -1,10 +1,19 @@
-import { Category } from "@discordx/utilities"
-import { ActionRowBuilder, ApplicationCommandOptionType, Attachment, AttachmentBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, CommandInteraction, EmbedBuilder, EmbedField, EmbedType, GuildMember, Message, MessageCollector, SelectMenuBuilder, StringSelectMenuBuilder, User, UserMention } from "discord.js"
-import { Client, SelectMenuComponent } from "discordx"
-import { injectable } from "tsyringe"
-
+import { 
+	ActionRowBuilder,
+	AttachmentBuilder,
+	ButtonBuilder, 
+	ButtonInteraction, 
+	ButtonStyle, 
+	Colors, 
+	CommandInteraction, 
+	EmbedBuilder, 
+	Role, 
+	StringSelectMenuBuilder
+} from "discord.js"
 import { Discord, Slash } from "@decorators"
 import { generateDiceAttachment, getColor, rollDice } from "@utils/functions"
+import { Category } from "@discordx/utilities"
+import { injectable } from "tsyringe"
 
 export enum CustomIds {
 	JOIN_GAME_BUTTON = "join-game-button",
@@ -12,12 +21,16 @@ export enum CustomIds {
 	CANCEL_GAME_BUTTON = "cancel-game-button",
 	SHOW_DICE_BUTTON = "show-dice-button",
 	WAGER_BUTTON = "wager-button",
-	BULLSHIT_BUTTON = "bullshit-button"
+	BULLSHIT_BUTTON = "bullshit-button",
+	QUANTITY_SELECT = "quantity-select",
+	DICE_SELECT = "dice-select",
+	SUBMIT_WAGER = "submit-wager"
 }
 
 interface Wager {
-	message: string | null;
 	calledBs: boolean;
+	quantity: string | null;
+	diceFace: string | null;
 	result: string | null;
 }
 
@@ -34,6 +47,13 @@ interface Game {
 	currentOrder: number;
 }
 
+interface DiscordButton {
+	id: string;
+	label: string;
+	style: ButtonStyle;
+	disabled: boolean;
+}
+
 @Discord()
 @injectable()
 @Category('General')
@@ -45,12 +65,20 @@ export default class OddsCommand {
 
 	embed: EmbedBuilder;
 
+	currentTurn: number;
+
+	role: Role | undefined;
+
+	messageId: string;
+
 	constructor() {
 		this.users = [];
+		this.currentTurn = 1;
 		this.game = {
 			currentWager: {
-				message: null,
 				calledBs: false,
+				quantity: null,
+				diceFace: null,
 				result: null,
 			},
 			currentOrder: 1
@@ -60,70 +88,68 @@ export default class OddsCommand {
 	@Slash({
 		name: 'liars-dice'
 	})
-	async oddsHandler(
+	async liarsDiceHandler(
 		interaction: CommandInteraction,
-		message: Message,
-		client: Client,
 	) {
+		/**
+		 * create attachment
+		 */
     const thumbnail = new AttachmentBuilder('./assets/images/liars_dice.png', { name: 'liars_dice.png' });
 
+		// this.role = await interaction.guild?.roles.create({
+		// 	name: `liars-dice-${interaction?.user?.id}`,
+		// 	color: Colors.DarkButNotBlack,
+		// 	reason: `Temporary role for ${interaction?.user?.username}'s game of liars dice.`,
+		// 	permissions: []
+		// })
+
 		/**
-		 * Add game initializer as first player
+		 * Add game host as first player
 		 */
-		this.users.push({
-			id: interaction.user.id,
-			username: interaction.user.username,
-			order: this.findNextOrder(),
-			dice: rollDice('5d6')[0],
-			wagers: []
-		});
+		this.setGameHost(interaction);
 
 		/**
 		 * Define game buttons
 		 */
-		const join = new ButtonBuilder()
-			.setLabel('Join game')
-			.setStyle(ButtonStyle.Primary)
-			.setCustomId('join-game-button');
-
-		const start = new ButtonBuilder()
-			.setLabel('Start game')
-			.setStyle(ButtonStyle.Success)
-			.setCustomId('start-game-button');
-		
-		const cancel = new ButtonBuilder()
-			.setLabel('Cancel')
-			.setStyle(ButtonStyle.Secondary)
-			.setCustomId('cancel-game-button');
-
-		const row = new ActionRowBuilder<ButtonBuilder>()
-			.addComponents(join, start, cancel);
+		const buttonRow = this.getEmbedButtons([
+			{ 
+				id: 'join-game-button', 
+				label: 'Join game', 
+				style: ButtonStyle.Primary,
+				disabled: false
+			}, 
+			{
+				id: 'start-game-button', 
+				label: 'Start game', 
+				style: ButtonStyle.Success,
+				disabled: false
+			}, 
+			{
+				id: 'cancel-game-button', 
+				label: 'Cancel game', 
+				style: ButtonStyle.Secondary,
+				disabled: false
+			}
+		]);
 
 		/**
 		 * Set game embed
 		 */
-    this.embed = new EmbedBuilder()
-			.setAuthor({
-				name: interaction.user.username,
-				iconURL: interaction.user.displayAvatarURL(),
-			})
-			.setTitle(`Started a game of Liar's Dice!`)
-			.setThumbnail('attachment://liars_dice.png')
-			.setColor(getColor('primary'))
-			.setFields({
-				name: `Player 1`,
-				value: `${interaction.user.username}`,
-				inline: true
-			})
-			.setFooter("\u3000".repeat(10) +"|" )
+    this.setDefaultGameEmbed(interaction);
 
+		/**
+		 * Send game embed
+		 */
 		const response = await interaction.reply({
 			embeds: [this.embed],
-			components: [row],
+			components: [buttonRow],
 			files: [thumbnail],
 		});
 
-		const collector = response?.createMessageComponentCollector({ time: 120000 });
+		/**
+		 * Define collector
+		 */
+		const collector = response?.createMessageComponentCollector();
 
 		// const collectorFilter = i => {
 		// 	i.deferUpdate();
@@ -133,50 +159,60 @@ export default class OddsCommand {
 		collector?.on('collect', async message => {
 			await message.deferUpdate();
 
-			if (message.customId === CustomIds.JOIN_GAME_BUTTON) {
-				const userId = message?.user?.id;
-				await this.handleJoinGame(userId, interaction, message);
-			}
+			const messageId = message?.customId;
+			const userId = message?.user?.id;
+			const user: Player | undefined = this.users.find( u => u.id === userId);
+			const didSelect = message.isStringSelectMenu();
+			const didPushButton = message.isButton();
 
-			if (message.customId === CustomIds.CANCEL_GAME_BUTTON) {
-				await interaction.deleteReply();
-				await interaction.channel?.send(`Liars dice has been cancelled by <@${message.user.id}>`);
-			}
-
-			if (message.customId === CustomIds.START_GAME_BUTTON) {
-				this.users.sort( (a, b) => a.order - b.order);
-				await this.handleStartGame(interaction);
-			}
-
-			if (message.customId === CustomIds.SHOW_DICE_BUTTON) {
-				this.handleShowDice(message);
-			}
-
-			if (message.customId === CustomIds.WAGER_BUTTON) {
-				const playerCount = this.users.length;
-				const diceCount = this.users.map( u => u.dice.length).reduce( (partialSum, a) => partialSum + a, 0 );
-
-				const quantityDropdown = new StringSelectMenuBuilder()
-					.setCustomId(`${message.user.id}-wager`)
-					.setPlaceholder('Select quantity')
-					.addOptions(...this.getSelectFields(diceCount))
-
-				const diceFaceDropdown = new StringSelectMenuBuilder()
-					.setCustomId(`${message.user.id}-dice`)
-					.setPlaceholder('Select dice face')
-					.addOptions(...this.getSelectFields(6))
-
-				const row = new ActionRowBuilder<StringSelectMenuBuilder>()
-					.addComponents(quantityDropdown, diceFaceDropdown);
-
-				message.update({
-					content: 'Pick your quantity and dice face to wager',
-					components: [row],
-				})
-			}
-
-			if (message.customId === CustomIds.BULLSHIT_BUTTON) {
-				console.log('bullshit');
+			switch(messageId) {
+				case CustomIds.JOIN_GAME_BUTTON:
+					await this.handleJoinGame(userId, interaction, message);
+					break;
+				case CustomIds.CANCEL_GAME_BUTTON:
+					if(user) {
+						await interaction.deleteReply();
+						await interaction.channel?.send(`Liars dice has been cancelled by <@${message.user.id}>`);
+					}
+					break;
+				case CustomIds.START_GAME_BUTTON:
+					if(user) {
+						this.users.sort( (a, b) => a.order - b.order);
+						await this.handleStartGame(interaction, user);
+					}
+					break;
+				case CustomIds.SHOW_DICE_BUTTON:
+					if(user) {
+						this.handleShowDice(message, user);
+					}
+					break;
+				case CustomIds.WAGER_BUTTON:
+					if(user) {
+						this.handleWagerSelect(message);
+					}
+					break;
+				case CustomIds.BULLSHIT_BUTTON:
+					if(didPushButton && user) {
+						this.game.currentWager.calledBs = true;
+						this.handleUserWager(message, user);
+					}
+					break;
+				case CustomIds.DICE_SELECT:
+					if(didSelect && user) {
+						this.game.currentWager.diceFace = message.values[0];
+					}
+					break;
+				case CustomIds.QUANTITY_SELECT:
+					if(didSelect && user) {
+						this.game.currentWager.quantity = message.values[0];
+					}
+					break;
+				case CustomIds.SUBMIT_WAGER:
+					if(didPushButton && user) {
+						this.handleUserWager(message, user);
+					}
+				default:
+					break;
 			}
 
 
@@ -184,6 +220,7 @@ export default class OddsCommand {
 
 		collector?.on('end', collected => {
 			console.log('ENDED: ', collected);
+			this.role!.delete('Game ended');
 		})
 	}
 
@@ -205,49 +242,70 @@ export default class OddsCommand {
 		await this.displayUsers(interaction);
 	}
 
-	private async handleStartGame(interaction: CommandInteraction) {
+	private async handleStartGame(interaction: CommandInteraction, user: Player) {
+
+		/**
+		 * Add game role to all players
+		 */
+		// const userList = interaction.guild?.members.cache.filter( (member) => this.users.some( async user => {
+		// 	if (user.id === member.user.id) {
+		// 		await member.roles.add(this.role!);
+		// 		return member;
+		// 	}
+		// } ) );
+
+		this.embed
+			.setTitle(`${this.users[0].username} it's your turn first!`)
+			.setFooter({
+				text: `Active turn: ${this.users[0].username}${this.users[1]?.username ? '\nNext turn: ' + this.users[1].username : ''}\nTotal dice count: ${this.getCurrentDiceCount()}`,
+			})
 
 		const fields = this.buildGameFields();
+
 		this.embed.setFields(...fields);
 
-		const showDice = new ButtonBuilder()
-			.setLabel('Show Dice')
-			.setStyle(ButtonStyle.Primary)
-			.setCustomId('show-dice-button');
+		const buttonRow = this.getEmbedButtons([
+			{
+				id: 'show-dice-button',
+				label: 'Show Dice',
+				style: ButtonStyle.Primary,
+				disabled: false
+			},
+			{
+				id: 'wager-button',
+				label: 'Wager',
+				style: ButtonStyle.Success,
+				disabled: false
+			},
+			{
+				id: 'liar-button',
+				label: 'Liar',
+				style: ButtonStyle.Danger,
+				disabled: true
+			},
+		]);
 		
-		const wager = new ButtonBuilder()
-			.setLabel('Wager')
-			.setStyle(ButtonStyle.Success)
-			.setCustomId('wager-button');
-		
-		const bullshit = new ButtonBuilder()
-			.setLabel('Bullshit')
-			.setStyle(ButtonStyle.Danger)
-			.setCustomId('bullshit-button');
-
-		const row = new ActionRowBuilder<ButtonBuilder>()
-			.addComponents(showDice, wager, bullshit);
-
 		await interaction.editReply({
 			embeds: [this.embed],
-			components: [row]
+			components: [buttonRow]
 		});
 	}
 
-	private async handleShowDice(message: any) {
-		const user: Player | undefined = this.users.find( u => u.id === message.user.id);
-		const { attachment } = await generateDiceAttachment([user?.dice]);
+	private async handleShowDice(message: any, user: Player | undefined) {
+		if(user) {
+			const { attachment } = await generateDiceAttachment([user?.dice]);
 
-		const embed = new EmbedBuilder()
-			.setColor(getColor('primary'))
-			.setTitle(`${message.user.username} rolls...`)
-			.setImage("attachment://currentDice.png")
-
-		await message.followUp({
-			embeds: [embed],
-			files: [attachment],
-			ephemeral: true
-		});
+			const embed = new EmbedBuilder()
+				.setColor(getColor('primary'))
+				.setTitle(`${message.user.username} rolls...`)
+				.setImage("attachment://currentDice.png")
+	
+			await message.followUp({
+				embeds: [embed],
+				files: [attachment],
+				ephemeral: true
+			});
+		}
 	}
 
 	private async displayUsers(interaction: CommandInteraction) {
@@ -271,6 +329,52 @@ export default class OddsCommand {
 		});
 	}
 
+	private async sendWagerMenu(message: CommandInteraction) {
+		const diceCount = this.getCurrentDiceCount();
+		const [row_one, row_two] = this.getWagerDropDowns(diceCount);
+		const submitButton = this.getEmbedButtons([
+			{
+				id: 'submit-wager',
+				label: 'Submit wager',
+				style: ButtonStyle.Success,
+				disabled: false
+			}
+		])
+
+		const { id } = await message.followUp({
+			content: 'Pick your quantity and dice to wager',
+			components: [row_one, row_two, submitButton],
+			ephemeral: true
+		})
+
+		this.messageId = id;
+	}
+
+	private async handleUserWager(message: ButtonInteraction, user: Player | undefined) {
+		console.log('did submit: ', this.game.currentWager.quantity, this.game.currentWager.diceFace)
+		message.deleteReply(this.messageId);
+		
+		if (this.game.currentWager.calledBs) {
+			this.embed.setTitle(`${user?.username} calls a ${this.users[this.game.currentOrder -1].username} liar!`)
+		} else {
+			this.embed.setTitle(`${user?.username} wagers ${this.game.currentWager.quantity} ${this.game.currentWager.diceFace}${Number(this.game.currentWager.quantity) > 1 ? "'s": ""}`)
+		}
+	}
+
+	private async handleWagerSelect(message: any) {
+		const player = this.users.find( user => user.id === message.user.id);
+
+		if (player && (this.currentTurn === player?.order)) {
+			this.sendWagerMenu(message);
+		}
+		else {
+			message.followUp({
+				content: "It's not your turn bro.",
+				ephemeral: true
+			});
+		}
+	}
+
 	private findNextOrder = () => {
 		const sorted = this.users.slice().sort((a: any, b: any) => a.order - b.order);
 		let previousOrder = 0;
@@ -289,9 +393,9 @@ export default class OddsCommand {
 		for( var i = 0; i < this.users.length; i++ ) {
 			const user = this.users[i];
 			fields.push({
-				name: `${user.username}`,
-				value: `Dice count: ${user.dice.length}`,
-				inline: true
+				name: user.username,
+				value: `Recent wager: ${this.getUsersLastWager(user)}\nTurn order: ${user.order}\nDice count: ${user.dice.length}`,
+				inline: i === 0 ? false : true
 			})
 		}
 		return fields;
@@ -302,11 +406,99 @@ export default class OddsCommand {
 		for (var i = 0; i < count; i++) {
 			fields.push({
 				label: `${i + 1}`,
-				description: `${i + 1}`,
 				value: `${i + 1}`,
 			})
 		}
 		return fields;
+	}
+
+	private setDefaultGameEmbed = (interaction: CommandInteraction) => {
+		this.embed = new EmbedBuilder()
+			.setAuthor({
+				name: `Host: ${interaction.user.username}`,
+				iconURL: interaction.user.displayAvatarURL(),
+			})
+			.setTitle(`${interaction.user.username} started a game of Liar's Dice!                \n\nPlayer list: `)
+			.setThumbnail('attachment://liars_dice.png')
+			.setColor(getColor('primary'))
+			.setFields({
+				name: `Player 1`,
+				value: `${interaction.user.username}`,
+				inline: true
+			})
+			.setFooter({
+				text: `Player count: ${this.users.length}`
+			})
+
+	}
+
+	private getEmbedButtons = (buttons: DiscordButton[]) => {
+		const embedButtons = buttons.map( button => (
+			new ButtonBuilder()
+				.setLabel(button.label)
+				.setStyle(button.style)
+				.setCustomId(button.id)
+				.setDisabled(button.disabled)
+		));
+
+		const row = new ActionRowBuilder<ButtonBuilder>()
+			.addComponents(embedButtons);
+
+		return row;
+	}
+
+	private setGameHost = (interaction: CommandInteraction) => {
+		this.users.push({
+			id: interaction.user.id,
+			username: interaction.user.username,
+			order: this.findNextOrder(),
+			dice: rollDice('5d6')[0],
+			wagers: []
+		});
+	}
+
+	private getWagerDropDowns = (diceCount: number) => {
+		const quantityDropdown = new StringSelectMenuBuilder()
+			.setCustomId('quantity-select')
+			.setPlaceholder('Select quantity')
+			.setMaxValues(diceCount)
+			.setMinValues(1)
+			.addOptions(...this.getSelectFields(diceCount))
+	
+		const diceFaceDropdown = new StringSelectMenuBuilder()
+			.setCustomId('dice-select')
+			.setPlaceholder('Select dice number')
+			.setMaxValues(6)
+			.setMinValues(1)
+			.addOptions(...this.getSelectFields(6))
+	
+		const row_one = new ActionRowBuilder<StringSelectMenuBuilder>()
+			.addComponents(quantityDropdown);
+
+		const row_two = new ActionRowBuilder<StringSelectMenuBuilder>()
+			.addComponents(diceFaceDropdown);
+
+		return [
+			row_one,
+			row_two
+		];
+	}
+
+	private getCurrentDiceCount = () => {
+		return this.users.map( u => u.dice.length).reduce( (partialSum, a) => partialSum + a, 0 );
+	}
+
+	private getUsersLastWager = (user: Player) => {
+		if(user.wagers.length) {
+			const wager = user.wagers.pop();
+			if(wager?.calledBs) {
+				return 'Called Liar!'
+			} else {
+				return `Bet ${wager?.quantity} ${wager?.diceFace}${wager?.quantity && (Number(wager.quantity) > 1) ? "'s": ""}`
+			}
+		} else {
+			return '';
+		}
 	}
 
 }
